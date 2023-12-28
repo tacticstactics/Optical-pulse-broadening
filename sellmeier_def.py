83af7d270dc904221d50b2c25bb8c6023ccccb5d
S <30459885+tacticstactics@users.noreply.github.com>
2023-12-28 14:25:44 +0900

    1

--- sellmeier_def.py
+++ sellmeier_def.py
@@ -1,18 +1,19 @@
 
 import numpy as np
+from scipy.constants import c 
 
-def sellmeiercore(wlcol = np.ones(64)):
+def sellmeiercore(wlcol):
 
-    ncorecol = np.ones(64)
-    Ncorecol = np.ones(64)
-    scorecol = np.ones(64)
+    ncorecol = wlcol
+    Ncorecol = wlcol
+    scorecol = wlcol
   
     return ncorecol, Ncorecol, scorecol
 
-def sellmeierclad(wlcol = np.ones(64)):
-
-    ncladcol = np.ones(64)
-    Ncladcol = np.ones(64)
-    scladcol = np.ones(64)
-
-    return ncladcol, Ncladcol, scladcol
+def sellmeierclad(wlcol):
+   
+   ncladcol =  np.sqrt(1 + 0.696 * wlcol**2/(wlcol)**2 - 0.004368) + 0.408 * wlcol**2/(wlcol**2 - 0.01395) + 0.896 * wlcol**2/(wlcol**2 - 97.93)
+   Ncladcol = wlcol
+   scladcol = wlcol
+   
+   return ncladcol, Ncladcol, scladcol
--- wl_freq_omega_def.py
+++ wl_freq_omega_def.py
@@ -1,11 +1,12 @@
 
 import numpy as np
+from scipy.constants import c 
 
-def wl_freq_omega(mm, stepfreq, startfreq):
+def wl_freq_omega(startfreq,stopfreq, mm):
 
-    freqcol = np.zeros(mm)
-    wlcol = np.zeros(mm)
-    omegacol = np.zeros(mm)
+    freqcol = np.linspace(startfreq,stopfreq, mm)
+    wlcol = c / freqcol
+    omegacol = 2*np.pi * freqcol
     kcol = np.zeros(mm)
     
     return freqcol, wlcol, omegacol, kcol
